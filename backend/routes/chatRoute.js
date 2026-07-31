d1e684b3d479ffd6e839a23e07837e5d38667050
Mudassir Ahmad <mudassir95255@gmail.com>
2026-07-31 05:40:06 -0600

    Aapki ki gayi changes ka chota sa message

--- backend/routes/chatRoute.js
+++ backend/routes/chatRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { getMessages, sendMessage } = require("../controllers/chatController");
-const { isAuthenticatedUser } = require("../middlewares/auth"); // Aapke project ka authentication middleware
+const { isAuthenticatedUser } = require("../middlewares/auth");
 
 const router = express.Router();
 
-router.route("/chats").get(isAuthenticatedUser, getMessages);
-router.route("/chat/send").post(isAuthenticatedUser, sendMessage);
+router.route("/chat")
+  .get(isAuthenticatedUser, getMessages)
+  .post(isAuthenticatedUser, sendMessage);
 
 module.exports = router;
